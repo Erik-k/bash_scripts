3ca108df476e7177567e516b89940eaaf4541a7f
Erik-k <enk@bu.edu>
2014-02-26 21:19:42 -0500

    added ability to check for a mounted usb drive as well as check for enough space available in a very rough way

--- backup.sh
+++ backup.sh
@@ -7,13 +7,10 @@
 # so I need to add some error checking to see if it should delete old
 # backups to make room for the new ones.
 
-# TODO: First check to see what the estimated size of the backup will 
-# be, then compare it with df to see if it'll fit. If it will, do it,
-# otherwise estimate whether deleting the oldest backup will make room.
-# Ideally it would scp the backup onto my other systems but that would
-# required hard coding my password in to a script. :-(
+#TODO:
+# Have it put the backups onto a USB key stuck in my router. Check 
+# available room on the stick to see if it should send me an email.
 
-SPACE_AVAIL=`df -h | head -n 2 | tail -n 1 | awk '{print $4}'
 # tar cannot estimate what the resulting archive will look like so the
 # most we could do is delete the previous backup, if it exists, if the 
 # available space is less than 250M. And I don't want this to delete
@@ -21,6 +18,27 @@ SPACE_AVAIL=`df -h | head -n 2 | tail -n 1 | awk '{print $4}'
 TIME=`date +"%Y-%b-%d"` 			 # Get timestamp
 FILENAME="home-backups-$TIME.tar.gz" # Create filename with timestamp
 SRCDIR="/home"
-DESDIR="/backups"
+if [ -d /mnt/USBstick ]
+then
+	# The preferred location: a usb stick
+	DESDIR="/mnt/USBstick/backups"
+	SPACE_AVAIL=`df -h | tail -n 1 | awk '{print $5}' | sed "s/%$//"`
+else
+	# The alternative location: somewhere else on the 'bone
+	DESDIR="/usr/backups"
+	SPACE_AVAIL=`df -h | tail -n 1 | awk '{print $5}' | sed "s/%$//"`
+fi
+
+# Note that "space_avail" is actually a misnomer because I started using
+# the percentage of space currently used. So we want to make sure its not
+# greater than about 90%
+
+if [ $SPACE_AVAIL -gt 90 ]
+then
+	echo "Not enough space available for backup."
+	echo "Amount already used is $SPACE_AVAIL %"
+	exit 1;
+fi
+
 tar -cpzf $DESDIR/$FILENAME $SRCDIR
 
