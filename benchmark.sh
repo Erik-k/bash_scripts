0ec2832e24339d06b052f3a39c2543ff4c09fb66
Erik <enk@bu.edu>
2014-04-30 18:53:34 -0400

    fixed an error with the variables, redirected outputs to devnull

--- benchmark.sh
+++ benchmark.sh
@@ -10,12 +10,14 @@
 #Add command line parameters for the size of the hugetestfile and the name 
 #of the results file and the number of times to run the whole script.
 #Example: $./benchmark.sh -size=500M -n=2 -o=new_results.txt
+#Also consider having this program output the average times of all the 
+#runs in the results.txt file.
 
-#$SIZE=1
-#$NUMBER_OF_RUNS=1
-#$OUTPUT_FILE=results.txt
+SIZE=1
+NUMBER_OF_RUNS=1
+OUTPUT_FILE=results.txt
 #For this project I'm running Tor in the background to simulate network usage.
-#$TOR=0
+TOR=0
 
 echo "-----------------" >> results.txt
 echo "Running a test:" >> results.txt
@@ -26,7 +28,10 @@ cat /proc/meminfo | head -n 2 | tail -n 1 >> results.txt
 #Is tor running in the background?
 #The -w arg for grep ensures we don't get false alarms from other processes
 #which have the letters tor, like *storage or *creator.
-if ps -e | grep -w tor; then $TOR=1; echo "FYI Tor is running in background.">>results.txt; fi
+if ps -e | grep -w tor &>/dev/null; 
+	then TOR=1; 
+	echo "FYI Tor is running in background." >> results.txt; 
+fi
 
 date >> results.txt
 
