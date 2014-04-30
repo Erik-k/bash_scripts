ed4071651233d4b319353650c0e97e8e7e684bdf
Erik <enk@bu.edu>
2014-04-30 01:05:35 -0400

    Check for Tor process when it runs and report available memory

--- benchmark.sh
+++ benchmark.sh
@@ -10,17 +10,37 @@
 #Add command line parameters for the size of the hugetestfile and the name 
 #of the results file and the number of times to run the whole script.
 #Example: $./benchmark.sh -size=500M -n=2 -o=new_results.txt
+
+#$SIZE=1
+#$NUMBER_OF_RUNS=1
+#$OUTPUT_FILE=results.txt
+#For this project I'm running Tor in the background to simulate network usage.
+#$TOR=0
+
 echo "-----------------" >> results.txt
 echo "Running a test:" >> results.txt
+
+#Display free memory at start of test, for reference:
+cat /proc/meminfo | head -n 2 | tail -n 1 >> results.txt
+
+#Is tor running in the background?
+#The -w arg for grep ensures we don't get false alarms from other processes
+#which have the letters tor, like *storage or *creator.
+if ps -e | grep -w tor; then $TOR=1; echo "FYI Tor is running in background.">>results.txt; fi
+
 date >> results.txt
+
 echo "Creating a 100M file:" >> results.txt
 echo >> results.txt
 (time dd if=/dev/urandom of=hugetestfile bs=100M count=1) 1>/dev/null 2>> results.txt
 echo >> results.txt
+
 echo "Gzipping and tarring that file:" >> results.txt
 (time tar czf hugetestfile.tgz hugetestfile) 1>/dev/null 2>> results.txt
 echo >> results.txt
+
 echo "Finished test." >> results.txt
 echo "----------------" >> results.txt
 
+#Cleanup our mess
 rm hugetestfile hugetestfile.tgz
